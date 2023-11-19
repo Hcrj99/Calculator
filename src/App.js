5882f4b478b572a4700e491ef46261f93122e5ce
hcrj99 <hcrj99@hotmail.com>
2023-11-19 18:26:30 -0500

    initial struct of the calculator

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import freeCodeCamplogo from './assets/img/freecodecamp-logo.png'
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
+      <section className='Logo__container'>
+        <img 
+        src={freeCodeCamplogo} 
+        className = 'Logo__container-logo'
+        alt = 'Logo of freecodecamp' />
+        <section className='Container__calculator'>
+
+        </section>
+      </section>
     </div>
   );
 }
