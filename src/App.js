3c1e1dbb81e401e8bb3d0a79a636ed6bcb9edf4e
hcrj99 <hcrj99@hotmail.com>
2023-11-21 20:10:27 -0500

    add logic class to button + template literals + trimEnd()

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import freeCodeCamplogo from './assets/img/freecodecamp-logo.png'
+import Button from './components/button';
 
 function App() {
   return (
@@ -11,7 +12,14 @@ function App() {
         alt = 'Logo of freecodecamp' />
       </section>
       <section className='Container__calculator'>
-
+        <div className='row'>
+          <Button>1</Button>
+          <Button>+</Button>
+        </div>
+        <div className='row'></div>
+        <div className='row'></div>
+        <div className='row'></div>
+        <div className='row'></div>
       </section>
     </div>
   );
